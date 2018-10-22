2c8a5985ff52adefeb14116bc73fdf691a4f7f39
s3842053 <123Potter>
2018-10-22 22:08:59 +0300

    /

--- asteroids/src/main/java/aoop/asteroids/model/server/Server.java
+++ asteroids/src/main/java/aoop/asteroids/model/server/Server.java
@@ -1,10 +1,9 @@
 package aoop.asteroids.model.server;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
@@ -53,10 +52,61 @@ public class Server implements Observer, Runnable {
         ObjectInputStream objStream = new ObjectInputStream(new ByteArrayInputStream(datagramPacket.getData()));
         PacketHeader packetHeader = (PacketHeader) objStream.readObject();
         switch (packetHeader) {
-            //case PACKET_LOGIN:
+            default:
+            case PACKET_LOGIN:
+                clientLogin(datagramPacket.getAddress());
+            case PACKET_SPECTATE:
+                spectating(datagramPacket.getAddress();
+                break;
+            case PACKET_DISCONNECT:
+                disconnectAll();
+                break;
         }
 
     }
+
+
+    /**
+     * Add all clients to a server
+     */
+    private void clientLogin(InetAddress ip) {
+        if(connectedClients.size()!= maxSpectators)
+            (this.connectedClients().add(new ConnectedClient(ip));
+            ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+            new = new ObjectOutputStream();
+
+    PacketHeader.PACKET_ACCEPTED;
+    }
+
+    /**
+     * Add all spectators to the server
+     */
+    private void spectating(InetAddress ip) {
+
+        ConnectedClient spectator = new ConnectedClient(ip);
+        this.connectedClients().add(spectator);
+    }
+
+    /**
+     * disconnects all spectators and diagramSocket from the server
+     * */
+    private void disconnectAll() {
+        for (ConnectedClient spectator : getSpectators()) {
+            this.getSpectators().remove(spectator);
+           // datagramSocket.disconnect();
+            datagramSocket.close();
+
+        }
+    }
+
+    private ArrayList<ConnectedClient> getSpectators() {
+        ArrayList<ConnectedClient> spectators;
+        spectators = (ArrayList<ConnectedClient>) this.connectedClients;
+        return spectators;
+    }
+
+
+
 
     @Override
     public void update(Observable o, Object arg) {

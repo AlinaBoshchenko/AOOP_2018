bcf6780fc99b6e2b0b1602bea46a615ff2f69cdb
s3842053 <123Potter>
2018-11-06 20:18:05 +0200

    - database+comments

--- asteroids/src/main/java/aoop/asteroids/model/packet/GamePacket.java
+++ asteroids/src/main/java/aoop/asteroids/model/packet/GamePacket.java
@@ -6,9 +6,15 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.logging.Logger;
 
+/**
+ * abstract class representing a game packet which is used for providing a game to Spectators and Joiners
+ */
 abstract public class GamePacket implements Serializable {
     transient private final static Logger logger = Logger.getLogger(GamePacket.class.getName());
 
+    /**
+     * sends packet
+     */
     public boolean sendPacket(DatagramSocket from, InetAddress inetAddress, int port) {
         ByteArrayOutputStream bStream = new ByteArrayOutputStream();
         ObjectOutput oo;
